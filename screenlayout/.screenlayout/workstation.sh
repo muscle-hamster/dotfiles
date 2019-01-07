aa61524aa4453a30f01d55791a953b9ef79183bd
Brock Morrison <brock.morrison@printerlogic.com>
2019-01-07 16:08:30 -0700

    updated dotfiles with added kitty config

---
+++ screenlayout/.screenlayout/workstation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-3-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-1-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 5768x1304 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 3848x1080 --rotate normal --output HDMI-1 --off --output DP-2 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1 --off
